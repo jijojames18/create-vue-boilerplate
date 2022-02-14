02f6bc0f5e08c6906acaca34df4ee083e85da660
Jijo James <jijojames18@gmail.com>
2022-02-14 10:55:02 +0530

    Boilerplate setup

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        corejs: 3,
+        debug: false,
+        useBuiltIns: 'entry',
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    ['@vue/app', { useBuiltIns: 'entry' }],
+  ],
+};
